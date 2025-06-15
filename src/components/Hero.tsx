305ca4ab76f85cb82fe56178d5f7f6000a923997
2myemy <lss8340@gmail.com>
2025-06-15 09:45:58 -0700

    initial commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <section className="h-screen flex flex-col items-center justify-center bg-white">
+      <h1 className="text-4xl font-bold">Hi, I'm Chloe Lee</h1>
+      <p className="mt-4 text-lg text-gray-600">
+        A developer passionate about meaningful tech.
+      </p>
+    </section>
+  )
+}
+
+export default Hero
